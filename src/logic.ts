c47b2f60a1bc1708eb409f45cf47d74f8af02dc7
Aman Meherally <amanmeherally@gmail.com>
2025-08-02 13:09:43 -0400

    Connected logic to home page

--- src/logic.ts
+++ src/logic.ts
@@ -1,5 +1,5 @@
 export function parseURL(query: string): string {
-  const search = `${query} site:chatgpt.com`;
+  const search = `${query} site:chatgpt.com/share`;
   const encoded = encodeURIComponent(search);
   return `https://duckduckgo.com/?q=${encoded}`;
 }
